806df6132962656559165f9c4242b053d049ace3
Aaron Kimbrell <awk73@msstate.edu>
2019-02-01 11:42:39 -0600

    Added templates
    base template and header\nav template
    Bootstrap is being pulled from the base template

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ main_blueprint = Blueprint('main', __name__)
 
 @main_blueprint.route('/')
 def index():
-    return render_template('main/index.html')
+    return render_template('main/index.jinja2')
