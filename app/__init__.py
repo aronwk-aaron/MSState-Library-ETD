44af8b9fca7b7ae1e1f5657d5951c6a84b9dc46b
Jordan Stremming <techzune@operontech.com>
2019-02-02 00:39:20 -0600

    PEP 8 formatting Google-style docstrings

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from config import configs
 
+
 def create_app(config_name='default'):
+    """Create and configure the Flask app
+
+    Args:
+        config_name (str): the configuration name, defined in config.py
+
+    Returns:
+        Flask: the configured app
+    """
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(configs[config_name])
@@ -11,11 +20,17 @@ def create_app(config_name='default'):
 
     return app
 
+
 # def register_extensions(app):
 #     db.init_app(app)
 #     migrate.init_app(app=app, db=db)
 #     ma.init_app(app)
 
 def register_blueprints(app):
+    """Register blueprints for Flask app
+
+    Args:
+        app (Flask): Flask app to register for
+    """
     from .main import main_blueprint
     app.register_blueprint(main_blueprint)
--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from flask import render_template, Blueprint
 
 main_blueprint = Blueprint('main', __name__)
 
+
 @main_blueprint.route('/')
 def index():
+    """Home/Index Page"""
     return render_template('main/index.jinja2')
