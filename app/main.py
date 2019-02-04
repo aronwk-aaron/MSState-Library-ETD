e494e445819ac95eb11d1689d68409707ef10382
Jordan Stremming <techzune@operontech.com>
2019-02-03 20:25:44 -0600

    Splash page template yay splashy splash!

--- app/main.py
+++ app/main.py
@@ -4,6 +4,12 @@ main_blueprint = Blueprint('main', __name__)
 
 
 @main_blueprint.route('/')
+def splash():
+    """Splash Page"""
+    return render_template('main/splash.jinja2')
+
+
+@main_blueprint.route('/home')
 def index():
     """Home/Index Page"""
     return render_template('main/index.jinja2')
