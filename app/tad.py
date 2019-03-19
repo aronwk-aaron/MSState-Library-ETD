812c9cb5b3bff5788206f68e849371e7d67e11c5
Jordan Stremming <jordan@jordanstremming.com>
2019-03-19 14:21:45 -0500

    TADs

--- app/tad.py
+++ app/tad.py
@@ -13,3 +13,30 @@ def new():
 def edit():
     """Edit submission page"""
     return render_template('tad/edit.jinja2', new=False)
+
+
+@tad_blueprint.route('/tad/<tad_id>/rev/<rev_id>')
+def revision(tad_id, rev_id):
+    """TAD revision view page"""
+    return render_template('tad/revision.jinja2',
+                           tad_id=tad_id,
+                           rev_id=rev_id)
+
+
+@tad_blueprint.route('/tad/<tad_id>')
+def view(tad_id):
+    """TAD revision view page"""
+    return render_template('tad/view.jinja2',
+                           tad_id=tad_id)
+
+
+@tad_blueprint.route('/tad/portfolio')
+def portfolio():
+    """User's TAD portfolio page"""
+    return render_template('tad/portfolio.jinja2')
+
+
+@tad_blueprint.route('/tad/catalog')
+def catalog():
+    """Catalog of TADs"""
+    return render_template('tad/catalog.jinja2')
